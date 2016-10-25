352ba82076889314f0e422b9dfd2d9c10b424da3
scott <scott@phin.co>
2016-10-25 11:50:01 -0700

    initial project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # node-cli-auth
+
 Simple module for authenticating a command line tool
---
+++ index.js
@@ -0,0 +1,7 @@
+( function(){
+
+	'use strict';
+
+	module.exports = require('./lib/node-cli-auth');
+
+})();
